a01726c3e46f033c0a2a4bda44ace4ae87545e07
mayrii101 <161327932+mayrii101@users.noreply.github.com>
2025-05-07 19:33:29 +0200

    bug?

--- AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
+++ AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d029f7d6e28cba3b2fdf34563c61295e124ddae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7427f871cf2daa5571b97803c09f1a493b3c55e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
