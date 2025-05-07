48f3ab6b62118ad6eef75071eae0b55dd5f9ac96
mayrii101 <161327932+mayrii101@users.noreply.github.com>
2025-05-07 15:41:46 +0200

    orders data fix

--- AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
+++ AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396099af16f484bc6d75c9569e79366216144b88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86eae44adc7e926c41ab4753ecc2e5b141b353d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
