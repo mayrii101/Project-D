67415bf2e7b2bb9aa9c73ebc75cb5962c8c02b70
mayrii101 <161327932+mayrii101@users.noreply.github.com>
2025-05-07 21:50:49 +0200

    .

--- AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
+++ AzureSqlConnectionDemo/obj/Debug/net8.0/AzureSqlConnectionDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3fb0eb742a84a0116546aad0de2a2868ff0eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643cfb227c31d148eacd53893afdc02e19517672")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureSqlConnectionDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
